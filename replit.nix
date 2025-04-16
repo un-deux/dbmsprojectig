e28bd7c59454447a297774af83c0b7f5fc9698a5
un-deux <158584638+un-deux@users.noreply.github.com>
2025-04-16 15:17:45 +0000

    Initial commit

--- replit.nix
+++ replit.nix
@@ -1,5 +1,25 @@
 {pkgs}: {
   deps = [
+    pkgs.tk
+    pkgs.tcl
+    pkgs.openjpeg
+    pkgs.libwebp
+    pkgs.libtiff
+    pkgs.libjpeg
+    pkgs.libimagequant
+    pkgs.lcms2
+    pkgs.freetype
+    pkgs.zlib
+    pkgs.openssl
+    pkgs.grpc
+    pkgs.c-ares
+    pkgs.rustc
+    pkgs.libiconv
+    pkgs.cargo
+    pkgs.pkg-config
+    pkgs.arrow-cpp
+    pkgs.libxcrypt
+    pkgs.cacert
     pkgs.glibcLocales
     pkgs.bash
   ];
